316add1d64314e922510ac7e62f70f44116e7c70
nitish1899 <nkword1899@gmail.com>
2024-12-03 17:58:18 +0530

    implement webSocket

--- MainNavigator.js
+++ MainNavigator.js
@@ -1,36 +1,5 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Landing from './screens/Landing';
-import Register from './screens/Register';
-import Login from './screens/Login';
-import ForgotPin from './screens/ForgotPin';
-import Welcome from './screens/Welcome';
-import Ourproduct from './screens/Ourproduct';
-import Visitordetails from './screens/Visitordetails';
-import Visitorchart from './screens/Visitorchart';
-import Visitorcomparechart from './screens/Visitorcomparechart';
-import BookingKit from './screens/BookingKit';
-import CheckoutButton from './screens/CheckoutButton';
-import BookingCheckout from './screens/BookingCheckout';
-import Recomanded from './screens/Recomanded';
-import AddToCart from './screens/AddToCart';
-import ViewCartItems from './screens/ViewCartItems';
-import SummaryCart from './screens/SummaryCart';
-import PaymentSuccess from './screens/PaymentSuccess';
-import Viewallorders from './screens/Viewallorders';
-import BillingSummary from './screens/BillingSummary';
-import PaymentPage from './screens/PaymentPage';
-import Congratulation from './screens/Congratulation';
-import PaymentSuccessnew from './screens/PaymentSuccessnew';
-import MultipleImageUpload from './screens/MediaImageUpload';
-import PremiumUser from './screens/PremiumUser';
-import KgvPaymentSuccess from './screens/KgvPaymantSuccess';
-import Register1 from './screens/Register1';
-import SpinFeature from './screens/SpinFeature';
-import TermsAndConditions from './screens/TermsAndConditions';
-import PremiumPayment from './screens/PremiumPayment';
-import PaymentImageUpload from './screens/PaymentImageUpload';
-import ContestpaymentImageUpload from './screens/ContestpaymentImageUpload';
-import PrimumpaymentImageUpload from './screens/PrimumpaymentImageUpload';
 import FlowSelect from './screens/FlowSelect';
 import Marketplace from './screens/Marketplace';
 import LoginCons from './screens/LoginCons';
@@ -48,6 +17,7 @@ import AddVehicleScreen from './screens/AddVehicleScreen';
 import GetVehicleScreen from './screens/GetVehicleScreen';
 import UpdateVehicleScreen from './screens/UpdateVehicleScreen';
 import GoogleMap from './screens/GoogleMap';
+import WebSocketScreen from './screens/WebSocketScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -61,61 +31,20 @@ export default () => {
             <Stack.Screen name="Marketplace" component={Marketplace} />
             <Stack.Screen name="LoginCons" component={LoginCons} />
             <Stack.Screen name="LoginVsp" component={LoginVsp} />
-            <Stack.Screen name="RegisterCons" component={RegisterCons} /> 
-            <Stack.Screen name="RegisterVsp" component={RegisterVsp} /> 
-            <Stack.Screen name="HomePreKyc" component={HomePreKyc} /> 
-            <Stack.Screen name="HomePreKycConsumer" component={HomePreKycConsumer} /> 
-            <Stack.Screen name="Kyc2" component={Kyc2} /> 
-            <Stack.Screen name="Trips" component={Trips} /> 
-            <Stack.Screen name="Profile" component={Profile} /> 
-            <Stack.Screen name="UpdateUserProfile" component={UpdateUserProfile} /> 
-            <Stack.Screen name="AddVehicleScreen" component={AddVehicleScreen} /> 
-          <Stack.Screen name="GoogleMap" component={GoogleMap} />
-          <Stack.Screen name="GetVehicleScreen" component={GetVehicleScreen} /> 
-            <Stack.Screen name="UpdateVehicleScreen" component={UpdateVehicleScreen} /> 
-           
-            
-
             <Stack.Screen name="RegisterCons" component={RegisterCons} />
             <Stack.Screen name="RegisterVsp" component={RegisterVsp} />
             <Stack.Screen name="HomePreKyc" component={HomePreKyc} />
+            <Stack.Screen name="HomePreKycConsumer" component={HomePreKycConsumer} />
             <Stack.Screen name="Kyc2" component={Kyc2} />
             <Stack.Screen name="Trips" component={Trips} />
             <Stack.Screen name="Profile" component={Profile} />
             <Stack.Screen name="UpdateUserProfile" component={UpdateUserProfile} />
-            <Stack.Screen name="ForgotPin" component={ForgotPin} />
-            <Stack.Screen name="Register" component={Register} />
-            <Stack.Screen name="Register1" component={Register1} />
-            <Stack.Screen name="Login" component={Login} />
-            <Stack.Screen name="Welcome" component={Welcome} />
-            <Stack.Screen name="Ourproduct" component={Ourproduct} />
-            <Stack.Screen name="Visitordetails" component={Visitordetails} />
-            <Stack.Screen name="Visitorchart" component={Visitorchart} />
-            <Stack.Screen name="Visitorcomparechart" component={Visitorcomparechart} />
-            <Stack.Screen name="Recomanded" component={Recomanded} />
-            <Stack.Screen name="AddToCart" component={AddToCart} />
-            <Stack.Screen name="ViewCartItems" component={ViewCartItems} />
-            <Stack.Screen name="SummaryCart" component={SummaryCart} />
-            <Stack.Screen name="BookingKit" component={BookingKit} />
-            <Stack.Screen name="CheckoutButton" component={CheckoutButton} />
-            <Stack.Screen name="BookingCheckout" component={BookingCheckout} />
-            <Stack.Screen name="PaymentPage" component={PaymentPage} />
-            <Stack.Screen name="PaymentSuccess" component={PaymentSuccess} />
-            <Stack.Screen name="Viewallorders" component={Viewallorders} />
-            <Stack.Screen name="BillingSummary" component={BillingSummary} />
-            <Stack.Screen name="Congratulation" component={Congratulation} />
-            <Stack.Screen name="PaymentSuccessnew" component={PaymentSuccessnew} />
-            <Stack.Screen name="MultipleImageUpload" component={MultipleImageUpload} />
-            <Stack.Screen name="PremiumUser" component={PremiumUser} />
-            <Stack.Screen name="KgvPaymentSuccess" component={KgvPaymentSuccess} />
-            <Stack.Screen name="SpinFeature" component={SpinFeature} />
-            <Stack.Screen name="TermsAndConditions" component={TermsAndConditions} />
-            <Stack.Screen name="PremiumPayment" component={PremiumPayment} />
-            <Stack.Screen name="PaymentImageUpload" component={PaymentImageUpload} />
-            <Stack.Screen name="ContestpaymentImageUpload" component={ContestpaymentImageUpload} />
-            <Stack.Screen name="PrimumpaymentImageUpload" component={PrimumpaymentImageUpload} />
+            <Stack.Screen name="AddVehicleScreen" component={AddVehicleScreen} />
+            <Stack.Screen name="GoogleMap" component={GoogleMap} />
+            <Stack.Screen name="GetVehicleScreen" component={GetVehicleScreen} />
+            <Stack.Screen name="UpdateVehicleScreen" component={UpdateVehicleScreen} />
+            <Stack.Screen name="WebSocketScreen" component={WebSocketScreen} />
          </Stack.Navigator>
       </>
    );
-
 }
---
+++ screens/WebSocketScreen.js
@@ -0,0 +1,98 @@
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, TextInput, Button, View } from 'react-native';
+import { io } from "socket.io-client";
+
+const WebSocketScreen = () => {
+    const [socket, setSocket] = useState(null);
+    const [messages, setMessages] = useState([]);
+    const [input, setInput] = useState('');
+
+    useEffect(() => {
+        // Connect to the Socket.IO server
+        const socketInstance = io("http://192.168.1.3:8000"); // Replace with your server URL
+        setSocket(socketInstance);
+
+        socketInstance.on("connect", () => {
+            console.log("Connected to the server!", socketInstance.id);
+        });
+
+        // socketInstance.on("newMessage", (message) => {
+        //     console.log("New message received:", message);
+        //     setMessages((prevMessages) => [...prevMessages, message.text]);
+        // });
+
+        socketInstance.on("hny", (message1) => {
+            console.log("New message received2:", message1);
+            setMessages((prevMessages) => [...prevMessages, message1.text]);
+        });
+
+        socketInstance.on("disconnect", () => {
+            console.log("Disconnected from the server.");
+        });
+
+        // Handle connection errors
+        socketInstance.on("connect_error", (error) => {
+            console.error("Connection error:", error);
+        });
+
+        // Clean up the connection on component unmount
+        return () => {
+            socketInstance.disconnect();
+        };
+    }, []);
+
+    const sendMessage = () => {
+        if (socket) {
+            socket.emit("createMessage", { text: input });
+            setInput(''); // Clear input field
+        }
+    };
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.heading}>Socket.IO Example</Text>
+            <View style={styles.messagesContainer}>
+                {messages.map((msg, index) => (
+                    <Text key={index} style={styles.message}>{msg}</Text>
+                ))}
+            </View>
+            <TextInput
+                style={styles.input}
+                placeholder="Type a message"
+                value={input}
+                onChangeText={setInput}
+            />
+            <Button title="Send" onPress={sendMessage} />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+        backgroundColor: '#fff',
+    },
+    heading: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 20,
+    },
+    messagesContainer: {
+        flex: 1,
+        marginBottom: 20,
+    },
+    message: {
+        fontSize: 16,
+        marginVertical: 5,
+    },
+    input: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginBottom: 10,
+        paddingHorizontal: 10,
+    },
+});
+
+export default WebSocketScreen;
